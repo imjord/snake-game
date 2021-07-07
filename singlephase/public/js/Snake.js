a3de19befdc044552cab3a1650be859db5008845
imjord <dean.jay.goat@gmail.com>
2021-07-07 11:11:42 -0600

    working snake

--- singlephase/public/js/Snake.js
+++ singlephase/public/js/Snake.js
@@ -1,12 +1,14 @@
 export default class Snake {
     constructor(scene){
         this.scene = scene;
+        this.lastMoveTime = 0;
+        this.moveInterval = 500;
         this.direction = Phaser.Math.Vector2.LEFT;
         // red box
         this.body = [];
-        this.body.push(this.scene.add.rectangle(0,0,16,16,0xff0000).setOrigin(0));
-        this.body = [];
+        this.body.push(this.scene.add.rectangle(100,100,16,16,0xff0000).setOrigin(0));
         this.body.push(this.scene.add.rectangle(100,100,16,16,0x0000ff).setOrigin(0));
+        this.body.push(this.scene.add.rectangle(100,100,16,16,0xffffff).setOrigin(0));
 
         scene.input.keyboard.on('keydown', e => {
             this.keydown(e);
@@ -33,10 +35,24 @@ export default class Snake {
         }
     }
     update(time){
+        if(time >= this.lastMoveTime + this.moveInterval){
+            this.lastMoveTime = time;
+            this.move();
+        }
+
+
+
+    }
+    move(){
+        for(let index = this.body.length - 1; index>0; index--){
+            this.body[index].x = this.body[index-1].x;
+            this.body[index].y = this.body[index-1].y;
+        }
+
         this.body[0].x += this.direction.x * 16;
         this.body[0].y += this.direction.y * 16;
 
-
+       
 
     }
 }
